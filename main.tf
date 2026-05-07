8ed6b9968861379223cce277d20aa470dbf9e1eb
ar1-prod-d_reconomy <ar1_prod_d@ourresource.onmicrosoft.com>
2026-05-07 20:26:39 +0300

    3t545

--- main.tf
+++ main.tf
@@ -77,6 +77,19 @@ resource "azapi_resource" "this" {
       body.properties.securityProfile.defender.securityGating,
       body.properties.storageProfile.diskCSIDriver,
       body.properties.workloadAutoScalerProfile,
+      body,
+      sensitive_body,
+      tags,
+      type,
+      ignore_null_property,
+      ignore_missing_property,
+      ignore_casing,
+      response_export_values,
+      replace_triggers_refs,
+      create_headers,
+      read_headers,
+      update_headers,
+      delete_headers,
     ]
   }
 }
