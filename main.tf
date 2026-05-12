c984c2c6f4795e2487019b046b82daf36fdbdb1a
ar1-prod-d_reconomy <ar1_prod_d@ourresource.onmicrosoft.com>
2026-05-12 21:21:51 +0300

    2025-10-01

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azapi_resource" "this" {
   location             = var.location
   name                 = var.name
   parent_id            = var.parent_id
-  type                 = "Microsoft.ContainerService/managedClusters@2026-03-01"
+  type                 = "Microsoft.ContainerService/managedClusters@2025-10-01"
   body                 = local.resource_body
   create_headers       = var.enable_telemetry ? { "User-Agent" : local.avm_azapi_header } : null
   delete_headers       = var.enable_telemetry ? { "User-Agent" : local.avm_azapi_header } : null
