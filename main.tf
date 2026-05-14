bb6d09d8258e7c1dc9955b402470d69c3b73a7bc
ar1-prod-d_reconomy <ar1_prod_d@ourresource.onmicrosoft.com>
2026-05-14 15:34:35 +0300

    ignore body.properties.agentPoolProfiles

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "azapi_resource" "this" {
 
   lifecycle {
     ignore_changes = [
-      #body.properties.agentPoolProfiles,
+      body.properties.agentPoolProfiles,
 
       body.properties.addonProfiles.azurepolicy.config,
       body.properties.azureMonitorProfile,
